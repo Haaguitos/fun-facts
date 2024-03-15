11c03f521003d6814d936a1fdb7167d9fb5e5fd1
Ígor Haag <igorhaagrod@gmail.com>
2024-03-15 11:01:52 -0300

    feat: translated about answers

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,28 +7,45 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class="flex flex-col gap-10 items-start max-w-prose mx-auto py-10 dark:text-custom-white">
+<div class="flex flex-col gap-10 items-start max-w-prose mx-auto pt-10 dark:text-custom-white">
     <h1 class="text-4xl font-bold">
 		{$_('about.title')}
 	</h1>
 
-	<p class="text-2xl font-bold">{$_('about.what')}<span class="font-heading text-3xl">fatonhos</span>?</p>
+	<p class="text-2xl font-bold">{$_('about.whatQuestion')}<span class="font-heading text-3xl">{" "}fatonhos</span>?</p>
 	<p class="text-2xl">
-		Lorem ipsum dolor sit amet. Est amet incidunt et voluptatem sunt sed Quis eaque. Aut quia praesentium non voluptate ipsam ut dolore maiores.
-		Non dolor enim ut pariatur quis eum ipsum rerum id autem necessitatibus ea molestiae beatae qui delectus numquam? 
+		{$_('about.whatAnswer')}
 	</p>
 
 	<p class="text-2xl font-bold">
-		{$_('about.truth')}
+		{$_('about.veracityQuestion')}
 	</p>
 	<p class="text-2xl">
-		Et aliquid quidem ut doloremque veniam quo laudantium quas cum voluptas culpa quo odio asperiores et sapiente rerum. 
+		{$_('about.veracityAnswer')}
 	</p>
 
 	<p class="text-2xl font-bold">
-		{$_('about.techs')}
+		{$_('about.techsQuestion')}
 	</p>	
-	<p class="text-2xl">
-		Id saepe accusamus qui veritatis necessitatibus vel sunt galisum eum provident omnis ea eveniet voluptate aut minima iste a officia repudiandae? Et nisi aspernatur quo amet natus et laudantium quia? 
+	<p class="inline-table text-2xl">
+		{$_('about.techsAnswerStart')}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://svelte.dev/"
+		  class="underline"
+        >
+          	Svelte
+		</a>
+		{$_('about.techsAnswerMiddle')}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://tailwindcss.com/"
+		  class="underline"
+        >
+          	tailwind
+        </a>
+		{$_('about.techsAnswerEnd')}
 	</p>
 </div>
